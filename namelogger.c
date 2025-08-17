2e3da2b0ae000bce91d6f8e7546a8d04a3d126d9
Matthew Labute <146600835+mlabute34@users.noreply.github.com>
2025-08-17 09:12:06 -0400

    Update namelogger.c
    Added basic error handling, also, coded this entirely from memory!! just to see if i could :) (i should mention i mean memory like in my brain not memory like in my computer)

--- namelogger.c
+++ namelogger.c
@@ -1,53 +1,59 @@
 #include <stdio.h>
 
-void addName(char name[50]);
-void readFile();
-void mainMenu();
+// Functions 
+// Add a name to file
+void addName();
+// View names from file 
+void viewNames(); 
+// Main Menu (Use Main?)
+void mainMenu(); 
+
 
 int main(){
-    mainMenu();
-    return 0; 
+    mainMenu(); 
+}
+
+void addName(){
+    FILE* fptr; 
+    char name[50]; 
+    printf("\nAdd a name: "); 
+    scanf("%s", name); 
+    fptr = fopen("log.txt", "a");
+    fprintf(fptr, "\n%s", name);
+    fclose(fptr); 
+}
+
+void viewNames(){
+    FILE* fptr; 
+    fptr = fopen("log.txt", "r"); 
+    char readFile[1000];
+    while(fgets(readFile, 1000, fptr)){
+        printf("%s", readFile); 
+    } 
+    fclose(fptr); 
 }
 
 void mainMenu(){
-    int input; 
-    char name[50]; 
-
-    printf("\n=== Main Menu ===\n");
-    printf("1. View Names\n"); 
-    printf("2. Add a name\n"); 
-    scanf("%d", &input);
-    switch(input){
-        case 1: 
-            readFile(); 
-            break;
-        case 2:
-            addName(name); 
-            break;
-        default:
-            printf("Invalid choice, try again.");
-            mainMenu(); 
+    int choice = 0; 
+    while(choice == 0){
+        printf("\n=== Main Menu ===\n");
+        printf("1. View names\n");
+        printf("2. Add a name\n");
+        printf("3. Exit the program\n");
+        scanf("%d", &choice); 
+        switch(choice){
+            case 1:
+                viewNames(); 
+                break;
+            case 2:
+                addName(); 
+                break;
+            case 3:
+                return; 
+            default:
+                printf("\n=== Invalid choice ===\n"); 
+                break; 
+        }
     }
-
-}
-
-void addName(char name[50]){
-    FILE *fptr; 
-    printf("Add name: "); 
-    scanf("%s", name); 
-    fptr = fopen("log.txt", "a"); 
-    fprintf(fptr, "%s\n", name); 
-    fclose (fptr); 
-    mainMenu();
-}
-
-void readFile(){
-    FILE *fptr; 
-    char line[100]; 
-    fptr = fopen("log.txt", "r"); 
-    while(fgets(line, 100, fptr)){
-        printf("\n%s", line); 
-    }
-    fclose (fptr); 
     mainMenu(); 
 }
